d65a5996958f4d0f30b779b6c9638e28691047e9
Gowthamkannappan <gowtham@techi.io>
2023-06-07 12:56:03 +0530

    Updated CSS

--- src/App.js
+++ src/App.js
@@ -1,53 +1,67 @@
 
-import './App.css';
-import { useState } from 'react';
-import {Task} from './Task'
+// import './App.css';
+import './style.css'
+import { useState } from 'react'
+import Task from './Task';
 
 function App() {
   const [todoList, setTodoList] = useState([]);
-  const [newTask, setNewTask] = useState('');
-
+  const [newTask, setNewTask] = useState("");
   const handleChange = (event) => {
     setNewTask(event.target.value);
-  };
+  }
 
   const addTask = () => {
     const task = {
       id: todoList.length === 0 ? 1 : todoList[todoList.length - 1].id + 1,
       taskName: newTask,
       completed: false,
-    };
-    const newTodoList = [...todoList, task];
-    setTodoList(newTodoList);
-  };
+    }
+    setTodoList([...todoList, task])
+  }
 
   const deleteTask = (id) => {
     setTodoList(todoList.filter((task) => task.id !== id));
-  };
-  const completeTask = (id) => {
-    setTodoList(
-      todoList.map((task) => {
-        if (task.id === id) {
-          return { ...task, completed: true };
-        } else {
-          return task;
-        }
-      })
-    );
-  };
+  }
+
+  const completeTask = (id) => (
+    setTodoList(todoList.map((task) => {
+      if (task.id === id) {
+        return { ...task, completed: true }
+      } else {
+        return task;
+      }
+
+    }
+    )
+    ))
+
 
   return (
-  <div className='App'>
-    <div className='addTask'>
-      <input placeholder='Enter Task' onChange={handleChange}/>
-      <button id='addButton' onClick={addTask}> Add Task </button>
+    <div className="App">
+
+      <div className='addtask'>
+        <p id='ptodo'>TODO LIST</p>
+        <input placeholder='write your todo list ...' onChange={handleChange} />
+        <button id='badd' onClick={addTask}>Add Task</button>
+
+
+      </div>
+
+      <div className="list">
+        <p id='listp'>List of Tasks</p>
+        {todoList.map((task) => (
+          <Task
+            key={task.id}
+            taskName={task.taskName}
+            id={task.id}
+            completed={task.completed}
+            deleteTask={deleteTask}
+            completeTask={completeTask}
+          />
+        ))}
+      </div>
     </div>
-    <div className='list'>
-      {todoList.map((task) => {
-        return <Task taskName = {task.taskName} id = {task.id} completed={task.completed} deleteTask = {deleteTask} completeTask={completeTask}/>;
-      })}
-    </div>
-  </div>
   );
 }
 
--- src/Task.js
+++ src/Task.js
@@ -1,12 +1,14 @@
-export const Task = (props) => {
-    return (
-        <div className="task">
-        
 
-
-          <h3 style={{backgroundColor : props.completed ? "green" : "white" }}> { props.taskName } </h3>
-          <button id="cmplt" onClick={() => props.completeTask(props.id)}> Complete </button>
-          <button id="dlt" onClick={() => props.deleteTask(props.id)}> Delete Task </button>
+const Task= (props) =>{
+   return(
+    <div className="task"
+    style={{ backgroundColor : props.completed ? "lightgreen":"cyan"}}>
+        <h1>{props.taskName}</h1>
+        <div className="buttons">
+            <button id="completebtn" onClick={()=>props.completeTask(props.id)}>Complete</button>
+            <button id="deletebtn" onClick={()=>props.deleteTask(props.id)}>Delete</button>
         </div>
-    );
+    </div>
+   )
 }
+export default Task;
